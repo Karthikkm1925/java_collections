696371cb63151c625b29095d6b639e5c16440723
Karthikkm1925 <Karthikkm1925@gmail.com>
2025-11-03 22:29:49 +0530

    collection list started

---
+++ src/collections/studentmangementsystem/StudentRegistration.java
@@ -0,0 +1,21 @@
+package collections.studentmangementsystem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StudentRegistration {
+	
+	public static void main(String[] args) {
+		
+		List<String> students = new ArrayList<String>();
+		students.add("Karthik");
+		students.add("Priya");
+		students.add("Anaga");
+		students.add("Vanaja");
+		
+		System.out.println("Aftre adding students : "+ students);
+		
+		List<String> newbatch = Arrays.asList("");
+	}
+}
